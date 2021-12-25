e9097f05f2233700dc3e610cda6d19e9628cc7b0
Pablo Kurskii <pablo.kurskii@gmail.com>
2021-12-25 10:49:31 +0100

    powerset generator update

--- math/src/main/java/org/albistudio/settheory/PowerSetGenerator.java
+++ math/src/main/java/org/albistudio/settheory/PowerSetGenerator.java
@@ -1,10 +1,86 @@
 package org.albistudio.settheory;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
 /* *
-* https://www.baeldung.com/java-power-set-of-a-set
-* */
+ * https://www.baeldung.com/java-power-set-of-a-set
+ * */
 public class PowerSetGenerator {
 
-    private class PowerSet{
+
+    public static <T> Set<Set<T>> generate(Set<T> set) {
+        if (set.isEmpty()) return ImmutableSet.of(new HashSet<>());
+
+        Set<Set<T>> sets = producePS(set);
+        System.out.println(sets.size());
+        return sets;
+    }
+
+    /*recursive returns */
+    private static <T> Set<Set<T>> producePS(Set<T> set) {
+        if (set.isEmpty()) return new HashSet<>();
+        /*iteration over set and probably recursion*/
+        /*Set<T> original = new LinkedHashSet<>(set);
+
+        T element = original.iterator().next();
+        original.remove(element);
+        System.out.println("removed " + element);
+
+        Set<T> subsetWithoutElement = new LinkedHashSet<>(original);
+        subsetWithoutElement.forEach(x -> System.out.println("next element " + x));
+        Set<Set<T>> powerSetSubsetWithoutElement = new HashSet<>();
+        producePS(subsetWithoutElement);*/
+
+        T element = set.iterator().next();
+        Set<T> subSetWithoutElement = getSubSetWithoutElement(set, element);
+        Set<Set<T>> powerSetSubSetWithoutElement = producePS(subSetWithoutElement);
+        Set<Set<T>> powerSetSubSetWithElement = addElementToAll(powerSetSubSetWithoutElement, element);
+
+        Set<Set<T>> powerSet = new HashSet<>();
+        powerSet.addAll(powerSetSubSetWithoutElement);
+        powerSet.addAll(powerSetSubSetWithElement);
+        return powerSet;
+    }
+
+    private static <T> Set<Set<T>> addElementToAll(Set<Set<T>> powerSetSubSetWithoutElement, T element) {
+        Set<Set<T>> powersetSubSetWithElement = new HashSet<>();
+        for (Set<T> subsetWithoutElement : powerSetSubSetWithoutElement) {
+            Set<T> subsetWithElement = new HashSet<>(subsetWithoutElement);
+            subsetWithElement.add(element);
+            powersetSubSetWithElement.add(subsetWithElement);
+        }
+        return powersetSubSetWithElement;
+    }
+
+    private static <T>Set<T> getSubSetWithoutElement(Set<T> set, T element){
+        Set<T> original = new LinkedHashSet<>(set);
+        original.remove(element);
+        System.out.println("removed " + element);
+
+        Set<T> subsetWithoutElement = new LinkedHashSet<>(original);
+        subsetWithoutElement.forEach(x -> System.out.println("next element " + x));
+        return subsetWithoutElement;
+    }
+
+    private static class PowerSet<T> extends AbstractSet<Set<T>> {
+
+        public PowerSet() {
+            Set<T> set = new LinkedHashSet<>();
+        }
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
     }
 }
--- math/src/test/java/org/albistudio/settheory/PowerSetGeneratorTest.java
+++ math/src/test/java/org/albistudio/settheory/PowerSetGeneratorTest.java
@@ -1,4 +1,78 @@
 package org.albistudio.settheory;
 
-class PowerSetGeneratorTest {
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PowerSetGeneratorTest {
+
+    @Test
+    @DisplayName("Should return exact quantity of sets")
+    public void testCardinality() {
+        ImmutableSet<Integer> set = ImmutableSet.of(1, 2, 3);
+        var cardinality = set.size();
+
+//        Set<Set<Integer>> powerSet = Sets.powerSet(set);
+        Set<Set<Integer>> powerSet = PowerSetGenerator.generate(set);
+        assertThat(powerSet).hasSize(1 << cardinality);
+
+
+    }
+
+    @Test
+    @DisplayName("Should return singleton power set whose only element is the empty set")
+    public void testGenerateSingletonEmptySet() {
+//        PowerSet powerSet = PowerSetGenerator.generate(new HashSet<>());
+//        assertThat(powerSet).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("String presentation of power set should be equal to set string")
+    public void testStringsEquality() {
+
+        Set<Set<Integer>> set = new HashSet<>();
+        set.add(new HashSet<>());
+        set.add(ImmutableSet.of(1));
+        set.add(ImmutableSet.of(2));
+        set.add(ImmutableSet.of(1, 2));
+        System.out.println(set.size());
+        StringJoiner joiner = new StringJoiner(",", "{", "}");
+        joiner.add("{}")
+                .add("{1}")
+                .add("{2}")
+                .add("{1,2}");
+
+        //        PowerSet powerSet = PowerSetGenerator.generate(set);
+        assertEquals("{{},{1},{1},{1,2}}", joiner.toString());
+
+    }
+
+    @Test
+//    @Disabled
+    @DisplayName("Each element of power set must be paired with tested set (sets are bijective)")
+    public void testBijection() {
+
+        Set<Set<Integer>> set = new HashSet<>();
+        set.add(new HashSet<>());
+        set.add(ImmutableSet.of(1));
+        set.add(ImmutableSet.of(2));
+        set.add(ImmutableSet.of(1, 2));
+
+        /*todo to be replaced by PS*/
+        Set<Set<Integer>> set2 = new HashSet<>();
+        set2.add(new HashSet<>());
+        set2.add(ImmutableSet.of(1));
+        set2.add(ImmutableSet.of(2));
+        set2.add(ImmutableSet.of(1, 2));
+
+        assertThat(set).hasSize(set2.size());
+        assertThat(set).containsAll(set2);
+        assertThat(set2).containsAll(set);
+    }
 }
